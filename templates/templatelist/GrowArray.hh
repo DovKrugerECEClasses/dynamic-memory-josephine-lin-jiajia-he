8044b0120612b64f70228414d04e3e6357c689bc
josephine lin <uuie0820@gmail.com>
2018-12-09 17:35:00 -0500

    update templatelist

--- templates/templatelist/GrowArray.hh
+++ templates/templatelist/GrowArray.hh
@@ -2,50 +2,54 @@
 
 #include <iostream>
 
-template<typename T>
-class GrowArray {
+template <typename T>
+class GrowArray{
 private:
-	int used;
-	int capacity;
-	T* data;
-	void checkGrow() {
-		if (  )
-			return; // don't grow unless you have to
-		T* old = data;
-
-		//TODO: every time the list grows, double it!!!
-		data = new T[used+1]; // calls T::T()
-		for (int i = 0; i < used; i++)
-			data[i] = old[i];
-		used++;
-	}
+    T* data;
+    int used;
+    int capacity;
+    void checkGrow(){
+        if(used<capacity)
+            return;
+        T* old = data;
+        data =new T[used*2];
+        for(int i=0;i<used;i++){
+            data[i]=old[i];
+        }
+        used ++;
+    }
 public:
-	GrowArray() : used(0), capacity(0), data(nullptr) {}
-	GrowArray(int  initialCapacity)
-		: used(0), capacity(initialCapacity), data( ) //TODO: fix! {
-			}
-	
-	//TODO: add destructor, copy constructor, operator =
-	void addEnd(const T& v) {
-		checkGrow();
-		data[length-1] = v;
-	}
+    GrowArray():used(0), capacity(0), data(nullptr){}
+    GrowArray(int initialCapacity) : used(0), capacity(initialCapacity), data(new T[used]){}
+    ~GrowArray(){delete [] data;}
+    void addEnd(const T& v){
+        checkGrow();
+        data[used-1]=v;
+    }
 
-	T removeEnd() {
-    length--;
-		return data[length];
-	}
-	T operator [](int i) const {
-		return data[i];
-	}
+    T removeEnd(){
+        used--;
+        return data[used];
+    }
 
-	T& operator [](int i) {
-		return data[i];
-	}
+    T operator =(const T& a){
+        for(int i=0;i < a.used;i++){
+            this->data[i]=a.data[i];
+        }
+    }
 
-	friend std::ostream& operator <<(std::ostream& s, const GrowArray<T>& list) {
-    for (int i = 0; i < list.length; i++)
-			s << list[i] << ' ';
-		return s;
-	}
+    T& operator[](int i) {
+        return data[i];
+    }
+
+    T operator[](int i)const {
+        return data[i];
+    }
+
+    friend std::ostream& operator << (std::ostream& s, const GrowArray<T>& list){
+        for(int i=0;i<list.used;i++){
+            s<<list[i]<< ' ';
+        }
+        return s;
+    }
 };
--- templates/templatelist/TestGrowArray.cc
+++ templates/templatelist/TestGrowArray.cc
@@ -2,29 +2,31 @@
 #include "string"
 using namespace std;
 
-class Elephant {
+class Elephant{
 private:
-	string name;
+    string name;
 public:
-	Elephant(const string& name) : name(name) {}
-	Elephant() : name() {}
-	friend ostream& operator <<(ostream& s, const Elephant& e) {
-		return s << e.name;
-	}
+    Elephant():name(){}
+    Elephant(const string& name): name(name){}
+    friend ostream& operator << (ostream&s, const Elephant& e){
+        return s<<e.name;
+    }
 };
 
-int main() {
-	GrowArray<int> a;
-	//TODO:	GrowArray b(1000);
-	for (int i = 0; i < 10; i++)
-		a.addEnd(i);
 
-	a.removeEnd();
-	cout << a;
+int main(){
+    GrowArray<int> a;
+    //TODO: GrowArray b(1000)
+    GrowArray<int> b(1000);
+    for(int i=0;i<10;i++){
+        a.addEnd(i);
+    }
+    a.removeEnd();
+    cout << a <<endl;
+    cout << b <<endl;
 
-	GrowArray<Elephant> e;
-	e.addEnd(Elephant("Alice"));
-  e.addEnd(Elephant("Betty"));
-	cout << e << '\n';
+    GrowArray<Elephant> e;
+    e.addEnd(Elephant("Alice"));
+    e.addEnd(Elephant("Betty"));
+    cout << e<<"\n";
 }
-	
