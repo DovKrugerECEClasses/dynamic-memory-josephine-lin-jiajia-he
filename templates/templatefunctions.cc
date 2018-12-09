e096ccd605f8cf3fbe7819d917e3493e34189c70
josephine lin <uuie0820@gmail.com>
2018-12-08 19:03:55 -0500

    correct mistake in templates

--- templates/templatefunctions.cc
+++ templates/templatefunctions.cc
@@ -33,10 +33,12 @@ int main() {
 	int a[] = {5, 4, 3, 1, 7, 8, 9, 2, 1};
 	sort(a, sizeof(a)/sizeof(int));
 	print(a, sizeof(a)/sizeof(int));
+	cout << endl;
 	
 	double b[] = {5.5, 4.5, 1.2, 2.9, 9.2, 5.2};
 	sort(b, sizeof(b)/sizeof(double));
 	print(b, sizeof(b)/sizeof(double));
+	cout << endl;
 
 	cout << sum(a, sizeof(a)/sizeof(int)) << '\n';
 	cout << sum(b, sizeof(b)/sizeof(double)) << '\n';
@@ -44,5 +46,6 @@ int main() {
 	string c[] = {"yo", "ho", "test", "foo", "bar"};
 	sort(c, sizeof(c)/sizeof(string));
 	print(c, sizeof(c)/sizeof(string));
+	cout << endl;
 	cout << sum(c, sizeof(c)/sizeof(string)) << '\n';
 }
