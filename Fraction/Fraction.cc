4482bf06a0a99677d0d9ab0b3381b478d494d044
josephine lin <uuie0820@gmail.com>
2018-12-11 11:20:33 -0500

    update Fraction

--- Fraction/Fraction.cc
+++ Fraction/Fraction.cc
@@ -1,24 +1,24 @@
 #include "Fraction.hh"
 
-using namespace std;
-Fraction::Fraction(int n, int d) { // TODO: finish initializer list :
-  
+Fraction::Fraction(int n, int d) : num(n), den(d){ // TODO: finish initializer list :
+
 }
 
 //friend
 Fraction operator +(Fraction a, Fraction b) {
-	//  a. b.
+    return Fraction(a.num*b.den+b.num*a.den, a.den*b.den);
 }
 
 // member operator
 Fraction Fraction::operator -(Fraction f) {
-
+    return Fraction(num*f.den-f.num*den, den*f.den);
 }
 
-ostream& operator <<(ostream& s, Fraction b) {
-
+std::ostream& operator <<(std::ostream& s, Fraction b) {
+    return s << b.num <<"/"<< b.den;
 }
 
 //just here for illustration, you do not have to do anything
 void Fraction::simplify() {
 }
+
